50999ddf8574dddddb3e11d2f34f8c75ef9e2ec3
Levit Osei Kanner <lkanner21@gmail.com>
2019-09-02 10:02:22 +0000

    Initial Commit

---
+++ instagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instagramClone
+//
+//  Created by Levit Kanner on 02/09/2019.
+//  Copyright © 2019 Levit Kanner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
